47e014392577fd43fa309463d62b6a6676d10b9c
niguum <71183609+niguum@users.noreply.github.com>
2021-11-04 21:41:42 +0300

    Add files via upload

---
+++ Random quiz/question3.js
@@ -0,0 +1,289 @@
+const quiz = [
+  {
+    q: "1.Bir neçə mineraldan ibarət olan süxurlar necə adlanır?",
+    options: [
+      "Monomineral süxurlar",
+      "Mikromineral süxurlar",
+      "Makromineral süxurlar",
+      "Nanomineral süxurlar",
+      " Polimineral süxurlar ",
+    ],
+    answer: 3,
+  },
+  {
+    q: "2.İlkin minerallara aid nümunələr hansılardır?",
+    options: [
+      "Xalsedon, Opal",
+      "Kaolinit, Kalsit ",
+      "Siderit, Halit",
+      "Gips, Kalsit",
+      " Kvars, Mikalar",
+    ],
+    answer: 3,
+  },
+  {
+    q: "3.Verilənlərdən hansı törəmə minerallara aid deyil?",
+    options: ["Xalsedon", "Opal", "Kaolinit", "Siderit", "Feldşpatitlər  "],
+    answer: 3,
+  },
+
+  {
+    q: " 4.Yer qabığında en geniş yayılmış mineral hansıdır?",
+    options: [
+      "Kvars",
+      "Kalsit",
+      "Mikalar ",
+      "Gilli minerallar",
+      " Çöl şpatları ",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "5.Aşağıdakılardan hansı dəniz fasiya qrupu süxurlarına aid deyil?",
+    options: [" Lotoral ", "Nerit", "Batial", "Abissal", " Laqun"],
+    answer: 3,
+  },
+  {
+    q: " 6.Aşağıdakılardan hansı kontinent fasiya qrupu süxurlarına aiddir?",
+    options: ["Nerit", "Laqun", "Abissal", "Lotoral", "Kolluvial sıra "],
+    answer: 3,
+  },
+  {
+    q: "7.Narın dənəli qumdaşı dənələrinin ölçüsü nə qədərdir?",
+    options: [
+      " 0.01-0.1 mm",
+      " 0.25-0.50 mm",
+      " 0.5-1 mm",
+      " 0.2-0.4 mm",
+      " 0.1-0.25 mm",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "8.Ortadənəli qumdaşı dənələrinin ölçüsü nə qədərdir?",
+    options: [
+      " 0.5-1 mm",
+      " 0.01-0.1 mm",
+      " 0.2-0.4 mm",
+      " 0.1-0.25 mm",
+      " 0.25-0.50 mm",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "9.İridənəli qumdaşı dənələrinin ölçüsü nə qədərdir?",
+    options: [
+      " 0.75-1.5 mm",
+      " 1-2 mm",
+      " 0.4-0.5 mm",
+      " 0.25-0.5 mm",
+      " 0.5-1 mm ",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "10.Karbonat süxurları təşkil edən mineral hansıdır?",
+    options: ["Dolomit", "Barit ", "Pirit", "Halit", " Kalsit "],
+    answer: 3,
+  },
+  {
+    q:
+      "11.Neftin tərkibində olan heteroatomlu birləşmələrə aid olmayan variant hansıdır?",
+    options: ["Asvaltenlər", "Merkaptanlar", "Fenollar", "Anilin ", " Arenlər"],
+    answer: 3,
+  },
+
+  {
+    q:
+      "12.Qətranlı-asvaltenli maddələrin miqdarı 35%-ə çatan qatı neftlər necə adlanır?",
+    options: ["Trepel", "Saprolit", "Oroka", "Jelvak", " Malta"],
+    answer: 3,
+  },
+
+  {
+    q:
+      "13.Püskürmə süxurlarda başlıca süxur əmələ gətirən minerallar sırasına aid olmayan minerallar hansıdır?",
+    options: [
+      " Sadə silikatlar ",
+      "Alümosilikatlar",
+      "Hidroksid qrupu",
+      "Kvars",
+      " Serpentin qrupu",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "14.Layın tavanı ilə dabanı arasındakı ın qısa məsafə necə adlanır?",
+    options: [
+      "Nisbi qalınlıq",
+      "Görünən qalınlıq",
+      "Orta qalınlıq",
+      "Maksimal qalınlıq",
+      "Həqiqi qalınlıq",
+    ],
+    answer: 3,
+  },
+
+  {
+    q:
+      "15.Qalınlığı az, lakin üfüqi istiqamətdə böyük sahəyə yayılan horizont necə adlanır?",
+    options: [
+      "Aşağı horizont",
+      "Yuxarı horizont ",
+      "Dayaq horizontu",
+      "Məhsuldar horizont",
+      " İstinad horizontu ",
+    ],
+    answer: 3,
+  },
+
+  {
+    q:
+      " 16.Neft və qaz yataqlarının axtarışı və kəşfiyyatı hansı süxurlarda aparılır?",
+    options: [
+      "Maqmatik süxurlar",
+      "Metamorfik süxurlar",
+      "Törəmə süxurlar",
+      "Vulkan püskürmələrinin məhsulu olan süxurlar ",
+      " Çökmə süxurlar",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "17.Çökmə süxurlar necə yaranmışdır?",
+    options: [
+      "Qarşılıqlı təmas, istilik və təzyiq nəticəsində mağmatik süxurlardan",
+      "Vulkan püskürmələri nəticəsində",
+      "Maqmatik süxurların yer daxilində yüksək təzyiq və temperatura, aktiv kimyəvi maddələrin təsirinə uyraması nəticəsində",
+      "Yüksək hərarətli, son dərəcə közərmiş, qazlarla zəngin olan silikat ərintilərin soyuması və bərkiməsi nəticəsində",
+      "Maqmatik və metamorfik süxurların fiziki, kimyəvi və mexaniki proseslər nəticəsində parçalanmasından",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "18.Metamorfik süxurlara aid nümunə hansı variantdadır?",
+    options: ["Bazalt", "Diabaz ", "Absidon", "Pemza", " Mərmər "],
+    answer: 3,
+  },
+
+  {
+    q: "19.Üzvi çöküntülərə aid nümunə hansı variantdadır?",
+    options: ["Əhəngli tuf", "Mergel", "Siderit", "Dolomit", "Diatomit"],
+    answer: 3,
+  },
+
+  {
+    q:
+      "20.Hər bir nöqtəsində fiziki sabitlər (istilikkeçirmə, elektrik - keçirmə, elastiklik, sərtlik və s) eyni olan süxurlar necə adlanır?",
+    options: ["İzobar", "İzoxor", "İzohips ", " İzotermik", " İzotrop   "],
+    answer: 3,
+  },
+
+  {
+    q:
+      " 21.Dağ süxurlarının neft və qaz quyularının qazılmasına təsir göstərən əsas fiziki-mexaniki xassələrinə aid olmayan variant hansıdır?",
+    options: [
+      "Məsaməlilik",
+      "Keçiricilik",
+      "Möhkəmlik",
+      "Abrazivlik",
+      " Özlülük",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "22.Süxurların xüsusi çəkisinin vahidi hansıdır?",
+    options: [" kq/m² ", "kq/san⁻¹", "kq*m/san²", "q/sm²", " q/sm³ "],
+    answer: 3,
+  },
+
+  {
+    q: "23.BM-6 cihazı vasitəsilə sement məhlulunun hansı parametri ölçülür?",
+    options: [
+      "Xüsusi çəkisi",
+      "Həcmi",
+      "Özlülüyü",
+      "Tutuşma vaxtı",
+      " Su verməsi",
+    ],
+    answer: 3,
+  },
+
+  {
+    q:
+      "24.Neft əsaslı qazma məhlulunun əsas tərkib hissəni hansı maddə təşkil edir?",
+    options: [
+      " Oksidləşdirilmiş bitum ",
+      "Oksidləşdirilmiş parafin",
+      "Kaustik soda",
+      "Sönmüş əhəng",
+      " Dizel yanacağı ",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "25.Bentonitdən hansı məqsədlə qazma məhlulunda istifadə olunur?",
+    options: [
+      "Durulaşdırma",
+      "Korroziyaya qarşı",
+      " Köpüksöndürmə",
+      "Ağırlaşdırma",
+      " Qatılaşdırma ",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "26.Maqnetit sement məhluluna hansı məqsədlə əlavə olunan reagentdir?",
+    options: [
+      "Udulmanın qarşısını almaq üçün",
+      "Qoruyucu kəməri korrizoyadan qorumaq üçün",
+      "Özlülüyü artırmaq üçün",
+      "Dayaz quyularda keyfiyyətli sementləmə əldə etmək üçün",
+      " Yüksək xüsusi çəki əladə etmək üçün",
+    ],
+    answer: 3,
+  },
+
+  {
+    q: "27.Hansı halda geofiziki işlərin aparılması qadağan edilir?",
+    options: [
+      "Kəmərendirməyə hazırlıq zamanı",
+      "Sementin bərkiməsi zamanı",
+      "Qazma avadanlıqları nasaz olduqda",
+      "Qazma prosesi davam edərkən",
+      " Udulma baş verdikdə",
+    ],
+    answer: 3,
+  },
+
+  {
+    q:
+      "28.Dayaz quyularda (1200 m-ə qədər) uyğun qazma intervallarında qazma məhlulunun xüsusi çəkisi lay təzyiqindən neçə faizdən çox olmamalıdır?",
+    options: [" 3-5%", " 40-60%", " 35-45%", " 7-12%", " 10-15% "],
+    answer: 3,
+  },
+
+  {
+    q:
+      "29.Orta dərinlikli quyularda (1200-2500 m) uyğun qazma intervallarında qazma məhlulunun xüsusi çəkisi lay təzyiqindən neçə faizdən çox olmamalıdır?",
+    options: [" 25-35%", " 3-5%", " 40-50%", " 15-25%", " 5-10% "],
+    answer: 3,
+  },
+
+  {
+    q:
+      "30.Dərin quyularda (2500 m-dən çox) uyğun qazma intervallarında qazma məhlulunun xüsusi çəkisi lay təzyiqindən neçə faizdən çox olmamalıdır?",
+    options: [" 15-20%", " 8-14%", " 2-3%", " 11-18%", " 4-7%"],
+    answer: 3,
+  },
+];
